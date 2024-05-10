0cf72cb3142f3126a7f70bff4d6e3a2f870415b5
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-05-10 13:16:29 -0700

    Adicionando teste da issue https://github.com/DesignLiquido/portugol-studio/issues/21.

--- testes/interpretador.test.ts
+++ testes/interpretador.test.ts
@@ -546,6 +546,49 @@ describe('Interpretador (Portugol Studio)', () => {
                     expect(metodoVisitarExpressaoLimpa).toHaveBeenCalledTimes(1);
                 });
             });
+
+            describe('Uso com Bibliotecas', () => {
+                it('Util.sorteio', async () => {
+                    let _saidas: string[] = [];
+                    interpretador.funcaoDeRetornoMesmaLinha = (saida: string) => {
+                        _saidas.push(saida);
+                    }
+
+                    const retornoLexador = lexador.mapear([
+                        'programa',
+                        '{',
+                        '    inclua biblioteca Util --> util',
+                        '    funcao inicio() ',
+                        '    {',
+                        '        inteiro vetor[10]',
+                        '        // preenche o vetor',
+                        '        para (inteiro posicao = 0; posicao < 10; posicao++)',
+                        '        {',
+                        '            vetor[posicao] = util.sorteia(1, 100) // Sorteia um número e atribui à posição do vetor',
+                        '        }',
+                        '        // Exibe o vetor na ordem original',
+                        '        escreva ("Vetor na ordem original:\n")',
+                        '        para(inteiro posicao = 0; posicao < 10; posicao++)',
+                        '        {',
+                        '            escreva (vetor[posicao], " ")',
+                        '        }',
+                        '        // Exibe o vetor na ordem inversa',
+                        '        escreva ("\n\nVetor na ordem inversa:\n")',
+                        '        para(inteiro posicao = 9; posicao >=0; posicao--)',
+                        '        {',
+                        '            escreva (vetor[posicao], " ")',
+                        '        }',
+                        '    }',
+                        '}'
+                    ], -1);
+    
+                    const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+                    const retornoInterpretador = await interpretador.interpretar(retornoAvaliadorSintatico.declaracoes);
+
+                    expect(retornoInterpretador.erros).toHaveLength(0);
+                    expect(_saidas).toHaveLength(22);
+                });
+            });
         });
     });
 });
