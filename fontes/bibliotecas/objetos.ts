1303629708baf010ca4d30aa191ffa27cc9dbed0
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2024-06-27 10:37:54 -0400

    Implementação inicial da biblioteca objetos

---
+++ fontes/bibliotecas/objetos.ts
@@ -0,0 +1,216 @@
+import { parseString } from 'xml2js';
+
+type Objeto = { [key: string]: any };
+
+let cacheObjetos: Objeto[] = [];
+
+export const TIPO_INTEIRO = 1;
+export const TIPO_CADEIA = 2;
+export const TIPO_CARACTER = 3;
+export const TIPO_REAL = 4;
+export const TIPO_LOGICO = 5;
+export const TIPO_OBJETO = 6;
+export const TIPO_VETOR = 7;
+
+export async function criar_objeto_via_json(json: string): Promise<number> {
+    const obj = JSON.parse(json);
+    return cacheObjetos.push(obj) - 1;
+}
+
+export async function criar_objeto_via_xml(xml: string): Promise<number> {
+    const obj = parseXml(xml);
+    return criar_objeto_via_json(obj);
+}
+
+export async function criar_objeto(): Promise<number> {
+    return cacheObjetos.push({}) - 1;
+}
+
+export async function atribuir_propriedade(endereco: number, propriedade: string, valor: any): Promise<void> {
+    cacheObjetos[endereco][propriedade] = valor;
+}
+
+export async function obter_propriedade_tipo_inteiro(endereco: number, propriedade: string): Promise<number> {
+    const valor = cacheObjetos[endereco][propriedade];
+    if (typeof valor !== 'number') {
+        throw new Error('Tipo inválido');
+    }
+    return valor;
+}
+
+export async function obter_propriedade_tipo_real(endereco: number, propriedade: string): Promise<number> {
+    const valor = cacheObjetos[endereco][propriedade];
+    if (typeof valor !== 'number') {
+        throw new Error('Tipo inválido');
+    }
+    return valor;
+}
+
+export async function obter_propriedade_tipo_logico(endereco: number, propriedade: string): Promise<boolean> {
+    const valor = cacheObjetos[endereco][propriedade];
+    if (typeof valor !== 'boolean') {
+        throw new Error('Tipo inválido');
+    }
+    return valor;
+}
+
+export async function obter_propriedade_tipo_caracter(endereco: number, propriedade: string): Promise<string> {
+    const valor = cacheObjetos[endereco][propriedade];
+    if (typeof valor !== 'string' || valor.length !== 1) {
+        throw new Error('Tipo inválido');
+    }
+    return valor;
+}
+
+export async function obter_propriedade_tipo_cadeia(endereco: number, propriedade: string): Promise<string> {
+    const valor = cacheObjetos[endereco][propriedade];
+    if (typeof valor !== 'string') {
+        throw new Error('Tipo inválido');
+    }
+    return valor;
+}
+
+export async function obter_propriedade_tipo_objeto(endereco: number, propriedade: string): Promise<number> {
+    const valor = cacheObjetos[endereco][propriedade];
+    if (typeof valor !== 'object' || Array.isArray(valor)) {
+        throw new Error('Tipo inválido');
+    }
+    return cacheObjetos.push(valor) - 1;
+}
+
+export async function obter_propriedade_tipo_objeto_em_vetor(
+    endereco: number,
+    propriedade: string,
+    indice: number
+): Promise<number> {
+    const vetor = cacheObjetos[endereco][propriedade];
+    if (!Array.isArray(vetor) || typeof vetor[indice] !== 'object') {
+        throw new Error('Tipo ou índice inválido');
+    }
+    return cacheObjetos.push(vetor[indice]) - 1;
+}
+
+export async function obter_propriedade_tipo_caracter_em_vetor(
+    endereco: number,
+    propriedade: string,
+    indice: number
+): Promise<string> {
+    const vetor = cacheObjetos[endereco][propriedade];
+    if (!Array.isArray(vetor) || typeof vetor[indice] !== 'string' || vetor[indice].length !== 1) {
+        throw new Error('Tipo ou índice inválido');
+    }
+    return vetor[indice];
+}
+
+export async function obter_propriedade_tipo_logico_em_vetor(
+    endereco: number,
+    propriedade: string,
+    indice: number
+): Promise<boolean> {
+    const vetor = cacheObjetos[endereco][propriedade];
+    if (!Array.isArray(vetor) || typeof vetor[indice] !== 'boolean') {
+        throw new Error('Tipo ou índice inválido');
+    }
+    return vetor[indice];
+}
+
+export async function obter_propriedade_tipo_real_em_vetor(
+    endereco: number,
+    propriedade: string,
+    indice: number
+): Promise<number> {
+    const vetor = cacheObjetos[endereco][propriedade];
+    if (!Array.isArray(vetor) || typeof vetor[indice] !== 'number') {
+        throw new Error('Tipo ou índice inválido');
+    }
+    return vetor[indice];
+}
+
+export async function obter_propriedade_tipo_inteiro_em_vetor(
+    endereco: number,
+    propriedade: string,
+    indice: number
+): Promise<number> {
+    const vetor = cacheObjetos[endereco][propriedade];
+    if (!Array.isArray(vetor) || typeof vetor[indice] !== 'number') {
+        throw new Error('Tipo ou índice inválido');
+    }
+    return vetor[indice];
+}
+
+export async function obter_propriedade_tipo_cadeia_em_vetor(
+    endereco: number,
+    propriedade: string,
+    indice: number
+): Promise<string> {
+    const vetor = cacheObjetos[endereco][propriedade];
+    if (!Array.isArray(vetor) || typeof vetor[indice] !== 'string') {
+        throw new Error('Tipo ou índice inválido');
+    }
+    return vetor[indice];
+}
+
+export async function obter_tamanho_vetor_propriedade(endereco: number, propriedade: string): Promise<number> {
+    const vetor = cacheObjetos[endereco][propriedade];
+    if (!Array.isArray(vetor)) {
+        throw new Error('Tipo inválido');
+    }
+    return vetor.length;
+}
+
+export async function liberar_objeto(endereco: number): Promise<void> {
+    delete cacheObjetos[endereco];
+}
+export async function obter_json(endereco: number): Promise<string> {
+    const objeto = cacheObjetos[endereco];
+    if (!objeto) {
+        throw new Error('Não foi possível obter o JSON deste objeto.');
+    }
+    return JSON.stringify(objeto);
+}
+
+export async function contem_propriedade(endereco: number, propriedade: string): Promise<boolean> {
+    return cacheObjetos[endereco].hasOwnProperty(propriedade);
+}
+
+export async function tipo_propriedade(endereco: number, propriedade: string): Promise<number> {
+    const valor = cacheObjetos[endereco][propriedade];
+    if (typeof valor === 'string') {
+        return valor.length === 1 ? TIPO_CARACTER : TIPO_CADEIA;
+    }
+    if (typeof valor === 'number') {
+        return TIPO_INTEIRO;
+    }
+    if (typeof valor === 'boolean') {
+        return TIPO_LOGICO;
+    }
+    if (typeof valor === 'object' && !Array.isArray(valor)) {
+        return TIPO_OBJETO;
+    }
+    if (Array.isArray(valor)) {
+        return TIPO_VETOR;
+    }
+    throw new Error('Tipo de propriedade desconhecida');
+}
+
+function parseXml(xmlString :string) {
+    let resultString = "";
+  
+    parseString(
+      xmlString,
+      {
+        explicitArray: false,
+        explicitRoot: false,
+        mergeAttrs: true,
+      },
+      (err, result) => {
+        if (err) {
+          console.error("Erro processando XML:", err);
+          resultString = "Erro processando XML";
+        } else {
+          resultString = JSON.stringify(result);
+        }
+      }
+    );
+    return resultString;
+  }
---
+++ testes/bibliotecas/objetos.test.ts
@@ -0,0 +1,351 @@
+import {
+    criar_objeto_via_json,
+    criar_objeto_via_xml,
+    criar_objeto,
+    atribuir_propriedade,
+    obter_propriedade_tipo_inteiro,
+    obter_propriedade_tipo_real,
+    obter_propriedade_tipo_logico,
+    obter_propriedade_tipo_caracter,
+    obter_propriedade_tipo_cadeia,
+    obter_propriedade_tipo_objeto,
+    obter_propriedade_tipo_objeto_em_vetor,
+    obter_propriedade_tipo_caracter_em_vetor,
+    obter_propriedade_tipo_logico_em_vetor,
+    obter_propriedade_tipo_real_em_vetor,
+    obter_propriedade_tipo_inteiro_em_vetor,
+    obter_propriedade_tipo_cadeia_em_vetor,
+    obter_tamanho_vetor_propriedade,
+    liberar_objeto,
+    obter_json,
+    contem_propriedade,
+    tipo_propriedade,
+    TIPO_INTEIRO,
+    TIPO_CADEIA,
+    TIPO_CARACTER,
+    TIPO_REAL,
+    TIPO_LOGICO,
+    TIPO_OBJETO,
+    TIPO_VETOR,
+} from './../../fontes/bibliotecas/objetos';
+
+describe('Biblioteca de Objetos', () => {
+    describe('Criar objeto via JSON', () => {
+        it('Trivial', async () => {
+            const json = '{"nome": "Charlotte"}';
+            const index = await criar_objeto_via_json(json);
+            const nome = await obter_propriedade_tipo_cadeia(index, 'nome');
+            expect(nome).toBe('Charlotte');
+        });
+    });
+
+    describe('Criar objeto via XML', () => {
+        it('Trivial', async () => {
+            const xml = '<root><nome>Charlotte</nome></root>';
+            const index = await criar_objeto_via_xml(xml);
+            const nome = await obter_propriedade_tipo_cadeia(index, 'nome');
+            expect(nome).toBe('Charlotte');
+        });
+    });
+
+    describe('Criar objeto', () => {
+        it('Trivial', async () => {
+            const index = await criar_objeto();
+            expect(index).toBeGreaterThan(-1);
+        });
+    });
+
+    describe('Atribuir propriedade', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+        });
+
+        it('Trivial', async () => {
+            await atribuir_propriedade(endereco, 'idade', 25);
+            const idade = await obter_propriedade_tipo_inteiro(endereco, 'idade');
+            expect(idade).toBe(25);
+        });
+    });
+
+    describe('Obter propriedade do tipo inteiro', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+        });
+
+        it('Trivial', async () => {
+            await atribuir_propriedade(endereco, 'idade', 25);
+            const idade = await obter_propriedade_tipo_inteiro(endereco, 'idade');
+            expect(idade).toBe(25);
+        });
+    });
+
+    describe('Obter propriedade do tipo real', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+        });
+
+        it('Trivial', async () => {
+            await atribuir_propriedade(endereco, 'altura', 1.75);
+            const altura = await obter_propriedade_tipo_real(endereco, 'altura');
+            expect(altura).toBe(1.75);
+        });
+    });
+
+    describe('Obter propriedade do tipo logico', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+        });
+
+        it('Trivial', async () => {
+            await atribuir_propriedade(endereco, 'isAdmin', true);
+            const isAdmin = await obter_propriedade_tipo_logico(endereco, 'isAdmin');
+            expect(isAdmin).toBe(true);
+        });
+    });
+
+    describe('Obter propriedade do tipo caracter', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+        });
+
+        it('Trivial', async () => {
+            await atribuir_propriedade(endereco, 'inicial', 'A');
+            const inicial = await obter_propriedade_tipo_caracter(endereco, 'inicial');
+            expect(inicial).toBe('A');
+        });
+    });
+
+    describe('Obter propriedade do tipo cadeia', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+        });
+
+        it('Trivial', async () => {
+            await atribuir_propriedade(endereco, 'nome', 'Charlotte');
+            const nome = await obter_propriedade_tipo_cadeia(endereco, 'nome');
+            expect(nome).toBe('Charlotte');
+        });
+    });
+    describe('Obter propriedade do tipo objeto', () => {
+        it('Trivial', async () => {
+            const json = `
+            {
+            "character":{
+                "id": 1,
+                "name": "Charlotte Wiltshire",
+                "position": "Lead Protagonist",
+                "email": "charlotte.wiltshire@example.com"
+                }
+            }`;
+            const index = await criar_objeto_via_json(json);
+            const retrievedSubEndereco = await obter_propriedade_tipo_objeto(index, 'character');
+            const nestedProp = await obter_propriedade_tipo_cadeia(retrievedSubEndereco, 'name');
+            expect(nestedProp).toBe('Charlotte Wiltshire');
+        });
+    });
+
+    describe('Obter propriedade do tipo objeto em vetor', () => {
+        it('Trivial', async () => {
+            const json = `
+                {
+                    "characters": [
+                        {
+                            "id": 1,
+                            "name": "Charlotte Wiltshire",
+                            "position": "Lead Protagonist",
+                            "email": "charlotte.wiltshire@example.com"
+                        },
+                        {
+                            "id": 2,
+                            "name": "Scarlett Eyler",
+                            "position": "Supporting Character",
+                            "email": "scarlett.eyler@example.com"
+                        }
+                    ]
+                }
+            `;
+            const vetor = await criar_objeto_via_json(json);
+            const retrievedSubEndereco = await obter_propriedade_tipo_objeto_em_vetor(vetor, 'characters', 0);
+            const resultProp = await obter_propriedade_tipo_cadeia(retrievedSubEndereco, 'name');
+            expect(resultProp).toBe('Charlotte Wiltshire');
+        });
+    });
+
+    describe('Obter propriedade do tipo caracter em vetor', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+            await atribuir_propriedade(endereco, 'caracteres', ['A', 'B', 'C']);
+        });
+
+        it('Trivial', async () => {
+            const primeiroCaracter = await obter_propriedade_tipo_caracter_em_vetor(endereco, 'caracteres', 0);
+            expect(primeiroCaracter).toBe('A');
+
+            const segundoCaracter = await obter_propriedade_tipo_caracter_em_vetor(endereco, 'caracteres', 1);
+            expect(segundoCaracter).toBe('B');
+
+            const terceiroCaracter = await obter_propriedade_tipo_caracter_em_vetor(endereco, 'caracteres', 2);
+            expect(terceiroCaracter).toBe('C');
+        });
+    });
+
+    describe('Obter propriedade do tipo logico em vetorr', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+            await atribuir_propriedade(endereco, 'logicos', [true, false]);
+        });
+
+        it('Trivial', async () => {
+            const primeiroLogico = await obter_propriedade_tipo_logico_em_vetor(endereco, 'logicos', 0);
+            expect(primeiroLogico).toBe(true);
+
+            const segundoLogico = await obter_propriedade_tipo_logico_em_vetor(endereco, 'logicos', 1);
+            expect(segundoLogico).toBe(false);
+        });
+    });
+
+    describe('Obter propriedade do tipo real em vetor', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+            await atribuir_propriedade(endereco, 'reais', [1.1, 2.2, 3.3]);
+        });
+
+        it('Trivial', async () => {
+            const primeiroReal = await obter_propriedade_tipo_real_em_vetor(endereco, 'reais', 0);
+            expect(primeiroReal).toBe(1.1);
+
+            const segundoReal = await obter_propriedade_tipo_real_em_vetor(endereco, 'reais', 1);
+            expect(segundoReal).toBe(2.2);
+
+            const terceiroReal = await obter_propriedade_tipo_real_em_vetor(endereco, 'reais', 2);
+            expect(terceiroReal).toBe(3.3);
+        });
+    });
+
+    describe('Obter propriedade do tipo inteiro em vetor', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+            await atribuir_propriedade(endereco, 'inteiros', [1, 2, 3]);
+        });
+
+        it('Trivial', async () => {
+            const primeiroInteiro = await obter_propriedade_tipo_inteiro_em_vetor(endereco, 'inteiros', 0);
+            expect(primeiroInteiro).toBe(1);
+
+            const segundoInteiro = await obter_propriedade_tipo_inteiro_em_vetor(endereco, 'inteiros', 1);
+            expect(segundoInteiro).toBe(2);
+
+            const terceiroInteiro = await obter_propriedade_tipo_inteiro_em_vetor(endereco, 'inteiros', 2);
+            expect(terceiroInteiro).toBe(3);
+        });
+    });
+
+    describe('Obter propriedade do tipo cadeia em vetor', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+            await atribuir_propriedade(endereco, 'cadeias', ['um', 'dois', 'três']);
+        });
+
+        it('Trivial', async () => {
+            const primeiraCadeia = await obter_propriedade_tipo_cadeia_em_vetor(endereco, 'cadeias', 0);
+            expect(primeiraCadeia).toBe('um');
+
+            const segundaCadeia = await obter_propriedade_tipo_cadeia_em_vetor(endereco, 'cadeias', 1);
+            expect(segundaCadeia).toBe('dois');
+
+            const terceiraCadeia = await obter_propriedade_tipo_cadeia_em_vetor(endereco, 'cadeias', 2);
+            expect(terceiraCadeia).toBe('três');
+        });
+    });
+
+    describe('obter_tamanho_vetor_propriedade', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+            await atribuir_propriedade(endereco, 'numeros', [1, 2, 3]);
+        });
+
+        it('Trivial', async () => {
+            const tamanho = await obter_tamanho_vetor_propriedade(endereco, 'numeros');
+            expect(tamanho).toBe(3);
+        });
+    });
+
+    describe('Deve liberar um objeto', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+        });
+
+        it('Trivial', async () => {
+            await liberar_objeto(endereco);
+            await expect(obter_json(endereco)).rejects.toThrow();
+        });
+    });
+
+    describe('Deve retornar o JSON de um objeto', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+        });
+
+        it('Trivial', async () => {
+            await atribuir_propriedade(endereco, 'nome', 'Charlotte');
+            const json = await obter_json(endereco);
+            expect(json).toBe(JSON.stringify({ nome: 'Charlotte' }));
+        });
+    });
+
+    describe('Verificar se uma propriedade existe', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+        });
+
+        it('Trivial', async () => {
+            await atribuir_propriedade(endereco, 'nome', 'Charlotte');
+            const exists = await contem_propriedade(endereco, 'nome');
+            expect(exists).toBe(true);
+        });
+    });
+
+    describe('Obter tipo da propriedade', () => {
+        let endereco: number;
+
+        beforeEach(async () => {
+            endereco = await criar_objeto();
+        });
+
+        it('Trivial', async () => {
+            await atribuir_propriedade(endereco, 'idade', 25);
+            const tipo = await tipo_propriedade(endereco, 'idade');
+            expect(tipo).toBe(TIPO_INTEIRO);
+        });
+    });
+});
