bc65b2662968cf8cea699aee535d248ec015ae62
Aristidescosta <aristidiscosta200@gmail.com>
2024-07-07 19:08:04 +0100

    refactor: adicionando configurações do jest

--- jest.config.ts
+++ jest.config.ts
@@ -9,9 +9,9 @@ export default async (): Promise<Config.InitialOptions> => {
         coverageReporters: ['json-summary', 'lcov', 'text', 'text-summary'],
         moduleNameMapper: {
             // Se for utilizar módulos linkados, comentar a linha abaixo:
-            // '@designliquido/delegua/(.*)': '<rootDir>/node_modules/@designliquido/delegua/$1'
+            '@designliquido/delegua/(.*)': '<rootDir>/node_modules/@designliquido/delegua/$1'
             // E descomentar a linha abaixo:
-            '@designliquido/delegua/(.*)': '<rootDir>/node_modules/@designliquido/delegua/fontes/$1'
+            //'@designliquido/delegua/(.*)': '<rootDir>/node_modules/@designliquido/delegua/fontes/$1'
         },
     };
 };
